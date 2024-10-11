a1bcef5c2ec58b6a48106d14eae05a35159967fd
Yans <728644051@qq.com>
2024-10-11 16:52:48 +0800

    feat playerMovement
    playerMovement

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+
+    private InputSystem_Player playerInput;
+
+    private Vector2 playerDir;
+
+    public int playerSpeed = 200;
+
+    private void Awake()
+    {
+        playerInput = new();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    private void Update()
+    {
+        playerDir = playerInput.PlayerMove.Move.ReadValue<Vector2>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        playerRb.linearVelocity = playerSpeed * Time.fixedDeltaTime * playerDir;
+    }
+
+}
