27c63c108b7e204bc121917fce87ce66881bb980
Yans <728644051@qq.com>
2024-10-21 16:27:27 +0800

    feat 敌人移动逻辑 70%
    1. 敌人移动逻辑
    2. 待完善

---
+++ Assets/Scripts/Enemys/EnemyCharecter.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class EnemyCharecter : MonoBehaviour
+{
+    [Header("敌人最大血量")]
+    public int enemyMaxHealth;
+    [Header("敌人当前血量")]
+    public int enemyCurrentHealth;
+    [Header("敌人速度")]
+    public int enemySpeed;
+}
--- Assets/Scripts/Enemys/Minion/MinionGenericScript.cs
+++ Assets/Scripts/Enemys/Minion/MinionGenericScript.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(EnemyCharecter))]
 public class MinionGenericScript : MonoBehaviour
 {
     private GameObject _playerObj;
+
+    private EnemyCharecter _enemyCharecter;
+
+    private Rigidbody2D _rigidbody;
+
+    //移动目的地
+    private Vector3 _destination;
+    //到达目的地
+    private bool _arrived = false;
 
     [Header("攻击间隔/s")]
     public float enemyAttackInterval = 1f;
@@ -11,6 +21,14 @@ public class MinionGenericScript : MonoBehaviour
     public GameObject enemyBulletPerFab;
     [Header("敌人子弹速度")]
     public float enemyBulletSpeed;
+    [Header("距离玩家安全距离")]
+    public float farFromPlayer = 10f;
+    [Header("无规则闪避范围左")]
+    [Range(-10f, 0)]
+    public float evasionRangeLeft;
+    [Header("无规则闪避范围右")]
+    [Range(0, 10f)]
+    public float evasionRangeRight;
 
     private void Awake()
     {
@@ -19,6 +37,8 @@ public class MinionGenericScript : MonoBehaviour
         {
             Debug.Log("无法获取玩家");
         }
+        _enemyCharecter = transform.GetComponent<EnemyCharecter>();
+        _rigidbody = transform.GetComponent<Rigidbody2D>();
     }
 
     private void Start()
@@ -26,9 +46,39 @@ public class MinionGenericScript : MonoBehaviour
         StartCoroutine(EnemyAttack());
     }
 
+    private void FixedUpdate()
+    {
+        EnemyFollowPlayer();
+    }
+
+    /// <summary>
+    /// 敌人跟随策略
+    /// </summary>
     private void EnemyFollowPlayer()
     {
-
+        if (Vector3.Distance(_playerObj.transform.position, transform.position) >= 10)
+        {
+            //靠近玩家
+            _destination = _playerObj.transform.position;
+        }
+        else if (_arrived)
+        {
+            //无规则闪避
+            float evasionX = Random.Range(evasionRangeLeft, evasionRangeRight);
+            float evasionY = Random.Range(evasionRangeLeft, evasionRangeRight);
+            _destination = new Vector3(transform.position.x + evasionX, transform.position.y + evasionY);
+            _arrived = false;
+        }
+        else
+        {
+            //TODO 待完善
+        }
+        if (Vector3.Distance(transform.position, _destination) <= 1)
+        {
+            _arrived = true;
+        }
+        //移动
+        _rigidbody.linearVelocity = _enemyCharecter.enemySpeed * Time.fixedDeltaTime * ToDestinationDir(_destination);
     }
 
     /// <summary>
@@ -52,6 +102,16 @@ public class MinionGenericScript : MonoBehaviour
     private Vector3 ToPayersDir()
     {
         return (_playerObj.transform.position - transform.position).normalized;
+    }
+
+    /// <summary>
+    /// 计算到目的地的方向
+    /// </summary>
+    /// <param name="destination"></param>
+    /// <returns></returns>
+    private Vector3 ToDestinationDir(Vector3 destination)
+    {
+        return (destination - transform.position).normalized;
     }
 
     /// <summary>
