1cffd544e21b71bbd382b5a28e92c216084c2963
Yans <728644051@qq.com>
2024-10-25 18:05:32 +0800

    feat 优化伤害控制器30%

--- Assets/Scripts/Managers/DamageManager.cs
+++ Assets/Scripts/Managers/DamageManager.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class DamageManager : Singleton<DamageManager>
 {
+    [Header("伤害文字预制件")]
     public DamageNumber damageNumberPerfab;
+    [Header("伤害文字父级")]
+    public GameObject damageParent;
 
     private void OnEnable()
     {
@@ -20,18 +23,54 @@ public class DamageManager : Singleton<DamageManager>
         Debug.Log("当前伤害Tag: " + other.tag);
         if (other.tag.StartsWith("Player"))
         {
-            damageNumberPerfab.Spawn(other.transform.position, bulletDamage);
-            PlayerCharecter _playerCharecter = FindTopComponet<PlayerCharecter>(other);
-            if (_playerCharecter.playerCurrentWeaponHealth - bulletDamage <= 0)
-            {
-                _playerCharecter.playerCurrentHealth -= ((int)bulletDamage - _playerCharecter.playerCurrentWeaponHealth);
-                _playerCharecter.playerCurrentWeaponHealth = 0;
-            }
-            else
-            {
-                _playerCharecter.playerCurrentWeaponHealth -= (int) bulletDamage;
-            }
+            //伤害显示
+            DamageTextSpawn(other.transform, bulletDamage);
+            //伤害计算
+            DamageToPlayer(other, bulletDamage);
         }
+        if (other.tag.StartsWith("Enemy"))
+        {
+            //伤害显示
+            DamageTextSpawn(other.transform, bulletDamage);
+            //伤害计算
+            DamageToEnemys(other, bulletDamage);
+        }
+    }
+
+    private void DamageToEnemys(Collider2D other, float bulletDamage)
+    {
+        EnemyCharecter enemyCharecter = FindTopComponet<EnemyCharecter>(other);
+        //TODO 敌人伤害计算逻辑
+    }
+
+    /// <summary>
+    /// 对玩家造成伤害
+    /// </summary>
+    /// <param name="other">玩家目标</param>
+    /// <param name="bulletDamage">伤害</param>
+    private void DamageToPlayer(Collider2D other, float bulletDamage)
+    {
+        PlayerCharecter _playerCharecter = FindTopComponet<PlayerCharecter>(other);
+        if (_playerCharecter.playerCurrentWeaponHealth - bulletDamage <= 0)
+        {
+            _playerCharecter.playerCurrentHealth -= ((int)bulletDamage - _playerCharecter.playerCurrentWeaponHealth);
+            _playerCharecter.playerCurrentWeaponHealth = 0;
+        }
+        else
+        {
+            _playerCharecter.playerCurrentWeaponHealth -= (int)bulletDamage;
+        }
+    }
+
+    /// <summary>
+    /// 生成伤害文字显示
+    /// </summary>
+    /// <param name="trans">位置</param>
+    /// <param name="damage">伤害</param>
+    private void DamageTextSpawn(Transform trans, float damage)
+    {
+        DamageNumber damageNumber = damageNumberPerfab.Spawn(trans.position, damage);
+        damageNumber.PrewarmPool();
     }
 
     /// <summary>
